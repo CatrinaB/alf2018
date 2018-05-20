9b5bf2d6ad594522a393d54b74b6cf975fc62e54
Ioana Culic <ioana_mculic@yahoo.com>
2018-05-21 02:32:46 +0300

    function

--- Devoirs/assembly/verify/run_asm.sh
+++ Devoirs/assembly/verify/run_asm.sh
@@ -5,6 +5,6 @@ echo -n > "$3"
 while read inputstr
 do
     echo $inputstr > "file.keyboard"
-	java -classpath ../CPUSim4.0.9:../CPUSim4.0.9/richtextfx-fat-0.6.10.jar cpusim.Main -m ../alf_computer32.cpu -t "$1" -c < "file.keyboard" >> "$3"
+	was r "$1" < "file.keyboard" >> "$3"
 done < "$2"
 
